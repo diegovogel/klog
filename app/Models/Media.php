470663a98af55d9263fe6af847180d4235b73258
Diego Vogel <diego@birdboar.co>
2026-01-22 14:41:47 -0500

    Add Media model and factory

---
+++ app/Models/Media.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Media extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'filename',
+        'original_filename',
+        'mime_type',
+        'size',
+        'disk',
+        'path',
+        'type',
+        'metadata',
+        'order',
+        'memory_id',
+    ];
+}
---
+++ database/factories/MediaFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\MediaType;
+use App\Enums\MimeType;
+use App\Models\Media;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
+class MediaFactory extends Factory
+{
+    protected $model = Media::class;
+
+    public function definition(): array
+    {
+        return [
+            'filename' => $this->faker->uuid(),
+            'original_filename' => Str::slug($this->faker->words(3, true)),
+            'mime_type' => array_rand(MimeType::values()),
+            'size' => $this->faker->randomNumber(7, true),
+            'disk' => 'local',
+            'path' => $this->faker->word(),
+            'type' => array_rand(MediaType::values()),
+            'metadata' => null,
+            'order' => 0,
+            'memory_id' => $this->faker->randomNumber(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
