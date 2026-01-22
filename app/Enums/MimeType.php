7cbf482a7375bba0fb125f94123e3474e5c1c093
Diego Vogel <diego@birdboar.co>
2026-01-22 14:41:38 -0500

    Simplify MIME type enum case names

--- app/Enums/MimeType.php
+++ app/Enums/MimeType.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum MimeType: string
 {
-    case IMAGE_JPEG = 'image/jpeg';
-    case VIDEO_MP4 = 'video/mp4';
-    case AUDIO_MPEG = 'audio/mpeg';
+    case JPEG = 'image/jpeg';
+    case MP4 = 'video/mp4';
+    case MPEG = 'audio/mpeg';
 
     public static function values(): array
     {
