78108d7c80541065770be84eda84d5cf26d37a62
Diego Vogel <diego@birdboar.co>
2026-01-30 17:48:37 -0500

    Style login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,48 @@
 <x-layouts.app>
-    <h1 class="login-title">{{ config('app.name', 'Klog') }}</h1>
+    <div class="login-wrapper">
+        <h1 class="login-title">{{ config('app.name', 'Klog') }}</h1>
 
-    <form method="POST"
-          action="{{ route('login') }}">
-        @csrf
+        <form method="POST"
+              action="{{ route('login') }}">
+            @csrf
 
-        <div class="form-group">
-            <label for="email"
-                   class="form-label">Email</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                autocomplete="email"
-                required
-                autofocus
-                value="{{ old('email') }}"
-                class="form-input"
-            >
-            @error('email')
-            <p class="form-error">{{ $message }}</p>
-            @enderror
-        </div>
+            <div class="form-group">
+                <label for="email"
+                       class="form-label">Email</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    required
+                    autofocus
+                    value="{{ old('email') }}"
+                    class="form-input"
+                >
+                @error('email')
+                <p class="form-error">{{ $message }}</p>
+                @enderror
+            </div>
 
-        <div class="form-group">
-            <label for="password"
-                   class="form-label">Password</label>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                autocomplete="current-password"
-                required
-                class="form-input"
-            >
-            @error('password')
-            <p class="form-error">{{ $message }}</p>
-            @enderror
-        </div>
+            <div class="form-group">
+                <label for="password"
+                       class="form-label">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    required
+                    class="form-input"
+                >
+                @error('password')
+                <p class="form-error">{{ $message }}</p>
+                @enderror
+            </div>
 
-        <button type="submit"
-                class="btn-submit">Log in
-        </button>
-    </form>
+            <button type="submit"
+                    class="btn-submit">Log in
+            </button>
+        </form>
+    </div>
 </x-layouts.app>
